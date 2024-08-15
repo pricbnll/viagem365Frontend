3ddd85f1a4968324d7be57b8a2362d6d712abb76
pricbnll <pricbnll@gmail.com>
2024-08-15 14:50:29 -0300

    refactor: update dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,15 @@
 import Sidebar from "../../components/Sidebar";
-import "./dashboard.module.css";
+import styles from "./dashboard.module.css";
 
 function Dashboard() {
   return (
     <>
+    <div className={styles.container}>
       <h1>Meus destinos</h1>
+      <div className={styles.formDiv}>
       <Sidebar/>
+      </div>
+    </div>
     </>
   );
 }
