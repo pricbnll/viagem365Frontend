e62e4906494e0da8141cc64027e00e5dec630288
pricbnll <pricbnll@gmail.com>
2024-08-16 14:29:39 -0300

    feat: Mapa e marcadores no mapa do destino do usuário

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,36 @@
-// import styles from "./map.module.css";
+import styles from "./map.module.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import MarcadoresMap from "./MarcadoresMap";
+import PropTypes from "prop-types";
 
-function Map() {
+function Map({localidades}) {
+  console.log(localidades)
+  const position = [-27.614160537150866, -48.504904703694876];
+
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MarcadoresMap localidades={localidades} />
+      </MapContainer>
+    </div>
   );
 }
+Map.propTypes = {
+  localidades: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+      descricao: PropTypes.string.isRequired,
+      destino: PropTypes.string.isRequired,
+    })
+  ),
+};
 
 export default Map;
