286ab25a67455b80ca9b9e46e7a60de974ac646b
pricbnll <pricbnll@gmail.com>
2024-08-17 11:56:00 -0300

    styles: estilizando btn de deletar e editar

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import LocalCard from "../../components/LocalCard";
 import Map from "../../components/Map";
 import Sidebar from "../../components/Sidebar";
-import styles from "./dashboard.module.css"; 
-import Table from "react-bootstrap/Table"; 
+import styles from "./dashboard.module.css";
+import Table from "react-bootstrap/Table";
 
 function Dashboard() {
   const { isAuthenticated, user } = useContext(AuthContext);
@@ -42,7 +42,7 @@ function Dashboard() {
           throw new Error("Não há conexão com a database");
         }
         const data = await response.json();
-        console.log("Dados recebidos:", data); 
+        console.log("Dados recebidos:", data);
         setLocalidades(data || []);
       } catch (error) {
         console.error("Erro ao buscar os dados", error);
@@ -60,11 +60,27 @@ function Dashboard() {
   }
 
   const totalLocais = localidades.length;
-  // console.log(totalLocais)
 
   const handleRegister = () => {
     console.log("Botão clicado");
     navigate("/cadastroLocalidade");
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      alert(`Tentando deletar localidade com id: ${id}`);
+      const response = await fetch(`http://localhost:3333/localidade/${id}`, {
+        method: "DELETE",
+      });
+      console.log(`Resposta do servidor: ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`Erro ao deletar o destino: ${response.statusText}`);
+      }
+      setLocalidades(localidades.filter((localidade) => localidade.id !== id));
+    } catch (error) {
+      console.error("Erro ao deletar o destino", error);
+      setError("Erro ao deletar o destino");
+    }
   };
 
   return (
@@ -101,6 +117,7 @@ function Dashboard() {
                   <th>CEP</th>
                   <th>Latitude</th>
                   <th>Longitude</th>
+                  <th>Ações</th>
                 </tr>
               </thead>
               <tbody>
@@ -113,10 +130,13 @@ function Dashboard() {
                     <td>{localidade.cep}</td>
                     <td>{localidade.latitude}</td>
                     <td>{localidade.longitude}</td>
+                    <td>
+                      <Link className={styles.btnUpdate} to={`/atualizarDestino/${localidade.id}`}>Editar</Link> 
+                      <button className={styles.btnDelete} onClick={() => handleDelete(localidade.id)}>Deletar</button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
-              {/* <Link to={`/atualizarDestinos/${localidade.id}`}>Editar</Link> */}
             </Table>
           )}
           <span>Seus destinos no mapa</span>
